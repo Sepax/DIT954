943c40a31a747761926e51d8ff2932ce13dc53a4
Kiril <kiril.hule@gmail.com>
2023-01-25 16:24:45 +0100

    Javadoc comments, only creating the docs left in terminal

--- Labs/LAB-1/src/main/java/com/car/models/Saab95.java
+++ Labs/LAB-1/src/main/java/com/car/models/Saab95.java
@@ -2,23 +2,41 @@ package com.car.models;
 
 import java.awt.*;
 
+/**
+ * Saab95 is a concrete implementation of the abstract Car class, representing the model Saab95.
+ * It has a turbo feature that can be turned on or off, which affects the speed factor.
+ * It also implements the speedFactor, incrementSpeed and decrementSpeed methods.
+ */
 public class Saab95 extends Car {
 
     private boolean turboOn;
 
+/**
+ * Constructs a new Saab95 object with 2 doors, red color, engine power of 125, and model name of "Saab95".
+ * The turbo feature is initially off.
+ */
     public Saab95() {
         super(2, Color.red, 125, "Saab95");
         turboOn = false;
     }
-
+/**
+ * Turns the turbo feature on.
+ */
     public void setTurboOn() {
         turboOn = true;
     }
 
+/**
+ * Turns the turbo feature off.
+ */
     public void setTurboOff() {
         turboOn = false;
     }
 
+/**
+ * Returns the speed factor of the car, which is the engine power multiplied by 0.01 and the turbo factor.
+ * @return the speed factor of the car
+ */
     public double speedFactor() {
         double turbo = 1;
         if (turboOn) {
@@ -27,6 +45,10 @@ public class Saab95 extends Car {
         return this.enginePower * 0.01 * turbo;
     }
 
+/**
+ * Increments the current speed of the car by the specified amount, up to the maximum engine power.
+ * @param amount the amount to increase the speed of the car
+ */
     public void incrementSpeed(double amount) {
         double newSpeed = getCurrentSpeed() + speedFactor() * amount;
 
@@ -35,6 +57,10 @@ public class Saab95 extends Car {
         }
     }
 
+/**
+ * Decrements the current speed of the car by the specified amount, down to a minimum of 0.
+ * @param amount the amount to decrease the speed of the car
+ */
     public void decrementSpeed(double amount) {
         double newSpeed = getCurrentSpeed() - speedFactor() * amount;
         
--- Labs/LAB-1/src/main/java/com/car/models/Volvo240.java
+++ Labs/LAB-1/src/main/java/com/car/models/Volvo240.java
@@ -2,18 +2,38 @@ package com.car.models;
 
 import java.awt.*;
 
+/**
+ * Volvo240 is a concrete implementation of the abstract Car class, representing the car model Volvo240.
+ * It has a trim factor of 1.25 and implements the speedFactor, incrementSpeed and decrementSpeed methods.
+ */
 public class Volvo240 extends Car {
 
+/** 
+ *  The trim factor of the car(constant), which is used to calculate the speed factor.
+*/
     public static final double TRIM_FACTOR = 1.25;
 
+/**
+ * Constructs a new Volvo240 object with 4 doors, black color, engine power of 100, and model name of "Volvo240".
+ */
     public Volvo240() {
         super(4, Color.black, 100, "Volvo240");
     }
 
+/**
+ * Returns the speed factor of the car, which is the engine power multiplied by 0.01 and the trim factor.
+ *
+ * @return the speed factor of the car
+ */
     public double speedFactor() {
         return enginePower * 0.01 * TRIM_FACTOR;
     }
 
+/**
+ * Increments the current speed of the car by the specified amount, up to the maximum engine power.
+ *
+ * @param amount the amount to increase the speed of the car
+ */
     public void incrementSpeed(double amount) {
         double newSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount, enginePower);
 
@@ -22,6 +42,11 @@ public class Volvo240 extends Car {
         }
     }
 
+/**
+ * Decrements the current speed of the car by the specified amount, down to a minimum of 0.
+ *
+ * @param amount the amount to decrease the speed of the car
+ */
     public void decrementSpeed(double amount) {
         double newSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
 

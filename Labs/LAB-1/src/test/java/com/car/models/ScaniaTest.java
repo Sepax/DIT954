3f9f86cd95c18c24e6ddd19bcd9966425420c3ea
Kiril <kiril.hule@gmail.com>
2023-02-01 17:10:07 +0100

    Scania development + tests

--- Labs/LAB-1/src/test/java/com/car/models/ScaniaTest.java
+++ Labs/LAB-1/src/test/java/com/car/models/ScaniaTest.java
@@ -1,3 +1,26 @@
 
+
+package com.car.models;
+
+import org.junit.jupiter.api.Test;
+
+import javafx.scene.paint.Color;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScaniaTest {
+
+    @Test
+    void testScania() {
+        Scania scania = new Scania();
+        assertEquals(2, scania.getNrDoors());
+        assertEquals(Color.CYAN, scania.getColor());
+        assertEquals(200, scania.getEnginePower());
+        assertEquals("Scania", scania.getModelName());
+        assertEquals(0, scania.getFlatbedTilt());
+    }
+
+}
+
     
 

81dcad93d56734df590dd357b547afd39fb5701d
Gabriel Frattini <gabbefrattini@gmail.com>
2023-02-02 15:56:00 +0100

    car ferry

---
+++ Labs/LAB-1/src/main/java/com/car/interfaces/CarTransporter.java
@@ -0,0 +1,22 @@
+package com.car.interfaces;
+
+import java.util.Deque;
+
+import com.car.models.Car;
+import com.car.models.Hauler.RampState;
+
+public interface CarTransporter {
+
+  public RampState getRampState();
+
+  public void loadCar(Car car);
+
+  public void unloadCar();
+
+  public Deque<Car> getLoadedCars();
+
+  public void lowerRamp();
+
+  public void raiseRamp();
+
+}
--- Labs/LAB-1/src/main/java/com/car/models/CarFerry.java
+++ Labs/LAB-1/src/main/java/com/car/models/CarFerry.java
@@ -1,13 +1,55 @@
 package com.car.models;
 
+import java.awt.Color;
+import java.util.Deque;
+
+import com.car.interfaces.CarTransporter;
+import com.car.models.Hauler.RampState;
+
 /**
  * Carferry class
  */
 
 /*
- * TODO: Interface that can be used by carferry and Hauler
+ * Separera på Car och Vehicle i olika klasser
  */
 
-public class CarFerry {
+public class CarFerry extends Car implements CarTransporter {
+
+  public CarFerry() {
+    super(2, Color.ORANGE, 500, "CarFerry", Dir.NORTH, false);
+    ramp = RampState.RAISED;
+  }
+
+  private RampState ramp;
+  Deque<Car> cars;
+
+  public void loadCar(Car car) {
+    cars.addLast(car);
+  }
+
+  public void unloadCar() {
+    cars.removeFirst();
+  }
+
+  public Deque<Car> getLoadedCars() {
+    return cars;
+  }
+
+  public RampState getRampState() {
+    return ramp;
+  }
+
+  public void lowerRamp() {
+    if (currentSpeed == 0) {
+      ramp = RampState.LOWERED;
+    }
+  }
+
+  public void raiseRamp() {
+    if (currentSpeed == 0) {
+      ramp = RampState.RAISED;
+    }
+  }
 
 }
